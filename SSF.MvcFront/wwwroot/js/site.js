55ce01c065359d772c00c97345b7923525e4396b
DiegoLeandroBarrios <barriosdiego.dev@gmail.com>
2026-07-20 11:58:23 -0300

    feat(ui): add global sweetalert2 mixin configuration in site.js #5

--- SSF.MvcFront/wwwroot/js/site.js
+++ SSF.MvcFront/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Configuración de clases personalizadas para SweetAlert2 en todo el sistema
+const MySwal = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn custom-swal-btn-confirm',
+        cancelButton: 'btn custom-swal-btn-cancel btn-danger'
+    },
+    buttonsStyling: false // Desactiva el estilo por defecto de SweetAlert para usar nuestro CSS
+});
